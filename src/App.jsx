d78bf82c6ddd4320e60d5268582f3da2f870713b
OldsoulPhil <brenenshea@gmail.com>
2023-07-07 10:51:41 -0400

    trying to fix the git repository like Jon asked

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Header } from "./components/Header";
+import { Table } from "./components/Table";
+import { CharacterCards } from "./components/CharacterCards";
+import { characters } from "./data";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <section id="character-ratings">
+          <h4>Top Characters</h4>
+          <Table data={characters} />
+        </section>
+        <section id="character-cards">
+          <CharacterCards data={characters} />
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/CharacterCards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../css/card.css"
+
+export class CharacterCards extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <>
+        {data.map((character, index) => (
+          <div key={index} className="card">
+            <div className="card-titles">
+              <h3>{character.name}</h3>
+              <h4>{character.nickName}</h4>
+            </div>
+            <img src={character.imageUrl} alt="" />
+            <p>{character.background}</p>
+          </div>
+        ))}
+      </>
+    )
+  }
+}
